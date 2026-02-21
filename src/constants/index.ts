d639b78cd184fe0834c90702d2c06f59c249eb1e
Gemmuel <gemmueldelacruz@gmail.com>
2026-02-21 23:08:53 +0800

    update manifest version

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,19 +3,19 @@ export const FIVE_MINUTES = ONE_MINUTE * 5;
 export const ONE_HOUR = ONE_MINUTE * 60;
 
 export const WORKING_OPTIONS = [5, 10, 15, 20, 30, 45, 60].map((option) =>
-  (option * ONE_MINUTE).toString()
+  (option * ONE_MINUTE).toString(),
 );
 
 export const BREAK_OPTIONS = [3, 5, 10, 15].map((option) =>
-  (option * ONE_MINUTE).toString()
+  (option * ONE_MINUTE).toString(),
 );
 
 export const LONG_BREAK_OPTIONS = [1, 15, 20, 25, 30].map((option) =>
-  (option * ONE_MINUTE).toString()
+  (option * ONE_MINUTE).toString(),
 );
 
 export const ULTRA_FOCUS_MODE_OPTIONS = [25, 60, 120, 180, 240, 300].map(
-  (option) => (option * ONE_MINUTE).toString()
+  (option) => (option * ONE_MINUTE).toString(),
 );
 
 export const REVIEW_PAGE =
@@ -24,6 +24,6 @@ export const REVIEW_PAGE =
 export const RELEASE_NOTES_URL =
   "https://github.com/BulletOnli/pomodoro-grande/releases";
 
-export const APP_VERSION = "v2.7.0";
+export const APP_VERSION = "v2.7.1";
 
 export const FEEDBACK_FORM_URL = "https://forms.gle/SQ6JwgWiRv79FDPv5";
